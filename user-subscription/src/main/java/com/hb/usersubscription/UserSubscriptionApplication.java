e80558fb6613e5441b8da6ec3d98bc1fbccfe607
hamza <hamzabouziane941@gmail.com>
2024-03-28 21:02:10 +0000

    init user-subscription

---
+++ user-subscription/src/main/java/com/hb/usersubscription/UserSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.hb.usersubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSubscriptionApplication.class, args);
+	}
+
+}
